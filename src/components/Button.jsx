bb4c7d8dfae71271f03a51463a2f239ce78c77a8
AsrafuzzamanKhan <asrafuzzamankhan89@gmail.com>
2024-03-26 03:36:01 +0600

    hero

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+
+const Button = ({ label, iconURL }) => {
+    return (
+        <button className="flex justify-center items-center px-7 py-4 bg-coral-red leading-none text-lg border  font-montserrat border-coral-red rounded-full text-white">{label} <img className="ml-2 rounded-full h-5 w-5" src={iconURL} alt="" /></button>
+    )
+}
+
+export default Button
